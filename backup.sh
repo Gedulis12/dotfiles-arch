e9ff58741a67b4c411fb3012b9adc74208e524ed
gedulis12 <gediminas.repecka@gmail.com>
2022-05-30 16:40:18 +0300

    New backup 2022-05-30 16:40:18

--- backup.sh
+++ backup.sh
@@ -15,3 +15,7 @@ rsync -a $HOME/.xinitrc ./
 rsync -a $HOME/.vimrc ./
 rsync -a $HOME/.vim ./
 rsync -a $HOME/.tmux.conf ./
+
+git add .
+git commit -m "New backup `date +'%Y-%m-%d %H:%M:%S'`";
+git push origin main
